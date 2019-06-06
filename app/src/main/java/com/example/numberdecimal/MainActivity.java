eb76e4c60e2e5b80c3ea5fefe30b0ea94c177e01
kcfang <kcf4ng@gmail.com>
2019-06-06 11:28:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/numberdecimal/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.numberdecimal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private void SetPoint(final EditText editText) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                int DECIMAL_DIGITS = 1;//小數點後面一位
+                if( s.toString().contains(".")){
+                    if(s.length()-1-s.toString().indexOf(".") > DECIMAL_DIGITS){
+                        s = s.toString().subSequence(0,s.toString().indexOf(".")+DECIMAL_DIGITS+1);
+                        editText.setText(s);
+                        editText.setSelection(s.length());
+                    }
+                }
+                if (s.toString().trim().substring(0).equals(".")) {
+                    s = "0" + s;
+                    editText.setText(s);
+                    editText.setSelection(2);
+                }
+                if (s.toString().startsWith("0")
+                        && s.toString().trim().length() > 1) {
+                    if (!s.toString().substring(1, 2).equals(".")) {
+                        editText.setText(s.subSequence(0, 1));
+                        editText.setSelection(1);
+                        return;
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        InitialComponent();
+
+    }
+
+    private void InitialComponent() {
+    txt1 = findViewById(R.id.txt1);
+    SetPoint(txt1);
+
+    }
+
+
+
+    EditText txt1;
+}
